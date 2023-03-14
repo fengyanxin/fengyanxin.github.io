838f2b8baa08eaad2251d97ebfce72a2a90c12fb
pengh <ph18317192001@163.com>
2023-03-14 16:14:40 +0800

    Blog updated: 2023-03-14 16:14:40

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,22 @@
+#!/bin/bash
+DIR=`dirname $0`
+
+# Generate blog
 hexo clean
-
 hexo generate
+sleep 5
 
+# Deploy
 hexo deploy
+sleep 5
+
+# Push hexo code
+git add .
+current_date=`date "+%Y-%m-%d %H:%M:%S"`
+git commit -m "Blog updated: $current_date"
+
+sleep 2
+
+git push origin hexo
+
+echo "=====>Finish!<====="
