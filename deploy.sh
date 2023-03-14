cb7c206d3dd5c35a99c76dfa640aeb70b1fe57b9
pengh <ph18317192001@163.com>
2023-03-14 17:50:55 +0800

    【modify】Blog updated: 2023-03-14 17:50:55

--- deploy.sh
+++ deploy.sh
@@ -4,19 +4,21 @@ DIR=`dirname $0`
 # Generate blog
 hexo clean
 hexo generate
-sleep 5
+sleep 2
 
 # Deploy
 hexo deploy
 sleep 5
 
+echo "=====>Deploy finish!<====="
+
 # Push hexo code
 git add .
 current_date=`date "+%Y-%m-%d %H:%M:%S"`
-git commit -m "Blog updated: $current_date"
+git commit -m "【modify】Blog updated: $current_date"
 
 sleep 2
 
 git push origin hexo
 
-echo "=====>Finish!<====="
+echo "=====>Push finish!<====="
--- source/tags/index.md
+++ source/tags/index.md
@@ -3,4 +3,5 @@ title: 标签
 date: 2023-03-03 16:32:53
 type: "tags"
 layout: "tags"
+comments: false
 ---
